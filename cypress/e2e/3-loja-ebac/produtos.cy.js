8511dfbca1f95a4d5c61ff7abf026ca05e0a2d79
Bruno rafael <rafael.sales7x@gmail.com>
2024-04-03 19:10:22 -0300

    buscando da massa de dados

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -1,19 +1,45 @@
 /// <reference types="cypress"/>
+import produtosPage from "../../support/page-objects/produtos.page";
 
 describe('Funcionalidade: Produtos ', () => {
 
 beforeEach(() => {
-    cy.visit('produtos')
+    produtosPage.visitarUrl()
 });
 
     it('Deve selecionar um produto da lista', () => {
-        cy.get('.products > .row')
-            //.first()
-             //.last()
-             //.eq(2)
-             .contains('Abominable Hoodie')
-             .click()
-
+       produtosPage.buscarProdutoLista('Aero Daily Fitness Tee')
              cy.get('#tab-title-description > a').should('contain', 'Descrição')
     });
+
+it('Deve buscar um produto com sucesso', () => {
+    let produto  = 'Aero Daily Fitness Tee'
+    produtosPage.buscarProduto(produto)
+    cy.get('.product_title').should('contain', produto )
+});
+
+it('Deve visitar a página do produto', () => {
+    produtosPage.visitarProduto('Aero Daily Fitness Tee')
+    cy.get('.product_title').should('contain', 'Aero Daily Fitness Tee' )
+});
+
+it('Deve adicionar produto ao carrinho', () => {
+    let qtd = 2
+    produtosPage.buscarProduto('Aero Daily Fitness Tee')
+    produtosPage.addProdutoCarrinho('L', 'Yellow', qtd)
+    cy.get('.woocommerce-message').should('contain', qtd + ' × “Aero Daily Fitness Tee” foram adicionados no seu carrinho.')
+   });
+
+   it.only('Deve adicionar produto ao carrinho buscando da massa de dados', () => {
+     cy.fixture('produtos').then(dados => {
+        produtosPage.buscarProduto(dados[0].nomeProduto)
+        produtosPage.addProdutoCarrinho(
+            dados[0].tamanho,
+            dados[0].cor,
+            dados[0].quantidade)
+        cy.get('.woocommerce-message').should('contain', dados[0].nomeProduto)
+})
+
+  
+   });
 });
