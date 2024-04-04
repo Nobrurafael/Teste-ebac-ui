a0f79dd208a86233f072d4c584cf396d669cf474
Bruno rafael <rafael.sales7x@gmail.com>
2024-04-04 13:26:08 -0300

    mudança no gitignore

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'ooqwyq',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'http://lojaebac.ebaconline.art.br'
+    baseUrl: 'http://lojaebac.ebaconline.art.br',
+    video: true,
   },
 });
